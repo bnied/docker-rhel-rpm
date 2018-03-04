3e8676d4ea95cf15025e20d98127c780ccb0dca5
Benjamin Nied <spacewreckage@gmail.com>
2018-03-03 19:35:08 -0500

    add submit to copr step to post-build

--- scripts/build_ml_kernel.sh
+++ scripts/build_ml_kernel.sh
@@ -163,6 +163,8 @@ fi
 
 # If we built the RPMs successfully, report that
 if [ $? -eq 0 ]; then
+  # Now that we've tested the source RPM, we can submit it to copr
+  copr-cli build kernel-ml-aufs --nowait -r epel-$EL_VERSION-$MOCK_ARCH rpms/kernel-lt-aufs-$FULL_VERSION-1.$RPM_EL_VERSION.src.rpm
   if [ ! -d ~/RPMs/ml ]; then
     echo "Creating RPM directory..."
     mkdir -p ~/RPMs/ml 2>&1
